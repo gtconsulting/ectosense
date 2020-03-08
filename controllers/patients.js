94feefd395224642002ddb6e0436e60816809796
Gopi Malla <gopi@gtconsulting.in>
2020-03-08 09:18:28 +0530

    cloudinary integrated

--- controllers/patients.js
+++ controllers/patients.js
@@ -5,54 +5,21 @@ const UserController = require("./users");
 
 module.exports = {
 
-    createAppointment: async (reqObj, res) => {
+    uploadRecord: async (reqObj, resObj) => {
         // overwrite if the role is something else other than patient
-        reqObj.role = "patient";
-        let user = await User.findOne({ email: reqObj.email });
-        if(!user) {
-            user = await UserController.register(reqObj);
+        
+        recordObj = {
+            name: reqObj.name,
+            type: reqObj.recordType,
+            desc: reqObj.desc,
+            url: resObj.locals.recordObj.secure_url,
         }
-        
-        let appointmentObj = {
-            patient: user._id,
-            doctor: reqObj.doctor,
-            clinic: reqObj.clinic,
-            address: reqObj.address,
-            createdBy: res.locals.user
+        await User.updateOne(
+            { _id: resObj.locals.user },
+            { $push: { records: recordObj } }
+        );
+        return {
+            success: true
         }
-        let appointment = await Appointment.create(appointmentObj);
-        
-        return appointment;
-    },
-
-    getAppointments: async (patient, assistant = null, clinic = null) => {
-        let queryObj = {
-            patient: patient
-        };
-        // Use the same fun for both doctors & assistants
-        assistant ? queryObj['createdBy'] = assistant : '';
-        clinic ? queryObj['clinic'] = clinic : '';
-        let appointments = await Appointment.find(queryObj)
-        .populate("patient", "-password -records")
-        .populate("doctor", "-password")
-        .populate("clinic", "-doctors -assistants")
-        .sort({ "createdAt": -1 }).exec();
-        
-        return appointments;
-    },
-
-    getAppointment: async (appointment, assistant = null, clinic = null) => {
-        let queryObj = {
-            _id: appointment
-        };
-        // Use the same fun for both doctors & assistants
-        assistant ? queryObj['createdBy'] = assistant : '';
-        clinic ? queryObj['clinic'] = clinic : '';
-        let appointmentObj = await Appointment.findOne(queryObj)
-        .populate("patient", "-password -records")
-        .populate("doctor", "-password")
-        .populate("clinic", "-doctors -assistants");
-        
-        return appointmentObj;
     }
 }
--- routes/patients.js
+++ routes/patients.js
@@ -7,6 +7,22 @@ const passport = require("passport");
 const sendError = require("../services/sendError");
 const { auth } = require("../middlewares/auth");
 const assistantController = require("../controllers/assistants");
+const patientController = require("../controllers/patients");
+
+const multer = require("multer");
+const upload = multer({ dest: 'public/records' });
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+var cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});  
+
 
 // All the routes below this middleware will be protected with token
 router.use((req, res, next) => auth(req, res, next));
@@ -32,10 +48,22 @@ router.get("/appointment/:appointment", async (req, res, next) => {
     }
 });
 
-router.post("/record", async (req, res, next) => {
+router.post("/record", upload.single('record'), 
+    (req, res, next) => {
+        cloudinary.uploader.upload(req.file.path, { resource_type: 'auto' },
+            function(err, result) {
+                if(err){
+                    next(err);
+                }
+                res.locals.recordObj = result;
+                next();
+            }
+        );
+    },
+    async (req, res, next) => {
     try{
-        let appointment = await assistantController.getAppointment(res.locals.user);
-        res.send(appointment);
+        await patientController.uploadRecord(req.body, res);
+        res.send(res.locals.recordObj);
     }
     catch (error){
         sendError(error, req, res, next);
