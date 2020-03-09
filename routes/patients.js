246122034eba8ed2eadde496711b981dd36f3271
Gopi Malla <gopi@gtconsulting.in>
2020-03-09 13:11:42 +0530

    env heroku check

--- routes/patients.js
+++ routes/patients.js
@@ -17,6 +17,7 @@ dotenv.config();
 
 var cloudinary = require('cloudinary').v2;
 
+console.log("process heroku: ", process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET, process.env.CLOUDINARY_NAME);
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
